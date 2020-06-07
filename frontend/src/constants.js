085ae7b0ce7da8859580c51517f630b86a36b34a
Machineric <goodminkyo@gmail.com>
2020-06-07 14:20:13 +0900

    Add github action

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,4 +1,4 @@
-const TODOS_API_BASE_URL_PROD = 'http://todosoftoday-dev.eba-symdykcm.ap-northeast-2.elasticbeanstalk.com/v1/todos/'
+const TODOS_API_BASE_URL_PROD = 'http://todosoftoday-dev.ap-northeast-2.elasticbeanstalk.com/v1/todos/'
 const TODOS_APi_BASE_URL_DEV = 'http://127.0.0.1:8000/v1/todos/'
 const TODOS_API_BASE_URL = process.env.NODE_ENV === 'production' ? TODOS_API_BASE_URL_PROD : TODOS_APi_BASE_URL_DEV
 
